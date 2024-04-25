5aae69965af55583c5be75e3b3f149da6447648f
alperenkucukali <alperen171195@hotmail.com>
2024-04-25 11:56:26 +0300

    delete product

---
+++ src/Application/Features/Products/Commands/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Application.Features.Products.Commands.UpdateProduct;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Products.Commands.DeleteProduct
+{
+    public class DeleteProductHandler : IRequestHandler<DeleteProductCommand>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteProductHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+            await _productRepository.DeleteAsync(product!);
+            return Unit.Value;
+        }
+    }
+}
