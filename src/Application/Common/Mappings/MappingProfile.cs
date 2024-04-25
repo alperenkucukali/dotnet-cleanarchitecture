eae378db98eafa4993ae9301759c9baadb607e67
alperenkucukali <alperen171195@hotmail.com>
2024-04-25 12:07:49 +0300

    mapper

---
+++ src/Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.Products.Queries.GetProduct;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.CreatedTime, opt => opt.MapFrom(src => src.Created));
+        }
+    }
+}
