e5431c6cfb3891ac5899b667697781ed31ca6764
alperenkucukali <alperen171195@hotmail.com>
2024-04-25 23:56:49 +0300

    added docker compose

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -25,14 +25,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));    
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+    app.UseHsts();
 }
 
 await app.InitializeDatabaseAsync();
 
 app.UseHealthChecks("/health");
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
