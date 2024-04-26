98c7f5acbb2f3404fc6718625b8bae69dcfcc813
alperenkucukali <alperen171195@hotmail.com>
2024-04-26 12:11:10 +0300

    some test functions added

---
+++ tests/Application.Tests/Commands/CreateProductCommandHandlerTests.cs
@@ -0,0 +1,126 @@
+﻿using Application.Common.Interfaces;
+using Application.Features.Products.Commands.CreateProduct;
+using Bogus;
+using FluentAssertions;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests.Commands
+{
+    public class CreateProductCommandHandlerTests
+    {
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+
+        public CreateProductCommandHandlerTests()
+        {
+            _productRepositoryMock = new();
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailureResult_WhenNameIsNorOrEmpty()
+        {
+            var command = new CreateProductCommand()
+            {
+                Name = "",
+                Description = "description",
+                ImageUrl = "imageurl"
+            };
+
+            var handler = new CreateProductHandler(_productRepositoryMock.Object);
+
+            var result = await handler.Handle(command, default);
+
+            result.ShouldBe(0);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailureResult_WhenNameExceedCharacter()
+        {
+            var command = new CreateProductCommand()
+            {
+                Name = new Faker().Random.String2(129),
+                Description = "description",
+                ImageUrl = "imageurl"
+            };
+
+            var handler = new CreateProductHandler(_productRepositoryMock.Object);
+
+            var result = await handler.Handle(command, default);
+
+            result.ShouldBe(0);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailureResult_WhenDescriptionIsNorOrEmpty()
+        {
+            var command = new CreateProductCommand()
+            {
+                Name = "name",
+                Description = "",
+                ImageUrl = "imageurl"
+            };
+
+            var handler = new CreateProductHandler(_productRepositoryMock.Object);
+
+            var result = await handler.Handle(command, default);
+
+            result.ShouldBe(0);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailureResult_WhenDescriptionExceedCharacter()
+        {
+            var command = new CreateProductCommand()
+            {
+                Name = "name",
+                Description = new Faker().Random.String2(513),
+                ImageUrl = "imageurl"
+            };
+
+            var handler = new CreateProductHandler(_productRepositoryMock.Object);
+
+            var result = await handler.Handle(command, default);
+
+            result.ShouldBe(0);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailureResult_WhenImageUrlIsNorOrEmpty()
+        {
+            var command = new CreateProductCommand()
+            {
+                Name = "name",
+                Description = "description",
+                ImageUrl = ""
+            };
+
+            var handler = new CreateProductHandler(_productRepositoryMock.Object);
+
+            var result = await handler.Handle(command, default);
+
+            result.ShouldBe(0);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnFailureResult_WhenImageUrlExceedCharacter()
+        {
+            var command = new CreateProductCommand()
+            {
+                Name = "name",
+                Description = "description",
+                ImageUrl = new Faker().Random.String2(1025)
+            };
+
+            var handler = new CreateProductHandler(_productRepositoryMock.Object);
+
+            var result = await handler.Handle(command, default);
+
+            result.ShouldBe(0);
+        }
+    }
+}
---
+++ tests/WebAPI.Tests/ProductsControllerTests.cs
@@ -0,0 +1,63 @@
+﻿using Application.Features.Products.Commands.CreateProduct;
+using Application.Features.Products.Commands.UpdateProduct;
+using Bogus;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests
+{
+    public class ProductsControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+
+        public ProductsControllerTests()
+        {
+            _mediatorMock = new();
+        }
+
+        [Fact]
+        public async Task Controller_CreateProduct_Should_ReturnSuccessResult_WhenProductObjectIsValid()
+        {
+            var controller = new ProductsController(_mediatorMock.Object);
+
+            var command = new CreateProductCommand()
+            {
+                Name = "name",
+                Description = "description",
+                ImageUrl = "imageurl"
+            };
+
+            var result = await controller.Create(command);
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async Task Controller_UpdateProduct_Should_ReturnFailureResult_WhenProductIdIsNotMatch()
+        {
+            var controller = new ProductsController(_mediatorMock.Object);
+
+            var command = new UpdateProductCommand()
+            {
+                Id = 1,
+                Name = "qwe",
+                Description = "description",
+                ImageUrl = "imageurl"
+            };
+
+            var result = await controller.Update(command, 2);
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(BadRequestResult));
+        }
+    }
+}
